64b146e076a063eb4208d560a1b1b563a75e26f0
Daryna Panfilova <107915770+Daryna-P@users.noreply.github.com>
2023-04-27 20:53:17 +0300

    Update fetch.js

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,8 +3,14 @@ export function fetchCountries(name) {
     return fetch (URL)
     .then(response => {
         if (!response.ok) {
+            if (response.status === 404) {
+                return [];
+              }
           throw new Error("Can't get data from server!");
         }
         return response.json();
       })
+      .catch(error => {
+        console.error(error);
+      });
     };
